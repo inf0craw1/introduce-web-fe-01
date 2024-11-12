4a4d25490e41c38493c56f366e97a1afacba8ede
inf0craw1 <inf0craw1@naver.com>
2024-11-12 20:47:23 +0900

    refactor: footer layouts and styles

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -1,81 +1,84 @@
-import React from 'react';
+import React from "react";
 
 import {
   LocationOn as LocationIcon,
   PhoneEnabled as PhoneIcon,
-  Print as PrintIcon,
   Drafts as EmailIcon,
-} from '@material-ui/icons';
+} from "@material-ui/icons";
 
-import './Footer.scss';
+import Logo from "assets/trackfarm_logo.png";
 
-const Footer = ({
-  isMobile,
-}) => {
+import "./Footer.scss";
+
+const Footer = ({ isMobile }) => {
   return (
-    <div className={`footer-container${isMobile ? ' isMobile' : ''}`}>
-      <div className='footer'>
-        <div>
-          <div className='footer-title orange-1'>
-            TRACK FARM
-          </div>
-          <div className='footer-description'>
-            <p>
-              COPYRIGHTⓒ2020
-              <br />
-              BY TRACKFARM CO, LTD. ALL RIGHTS RESERVED
-            </p>
-          </div>
+    <div className={`footer-container${isMobile ? " isMobile" : ""}`}>
+      <div className="footer">
+        <div className="footer-logo-container orange-1">
+          <img alt="logo" src={Logo} className="footer-logo" />
         </div>
-        <div>
-          <div className='footer-title'>
+        <div className="footer-description-container">
+          <div className="footer-description-title font-text-md">
             ADDRESS LIST
           </div>
-          <div className='footer-description'>
-            <p>
-              <LocationIcon fontSize='small' htmlColor='#cc9955' />
-              HeadOffice: Chungcheongnam-do Asan-si Baebangeup Heemangro 46beongil 45-11, 502
-            </p>
-            <p>
-              <LocationIcon fontSize='small' htmlColor='#cc9955' />
-              Laboratory: Gyeonggi-do Seongnam-si Sujeonggu Daewangpangyoro 815, Pangyo Startup-zone, 832
-            </p>
-            <p>
-              <LocationIcon fontSize='small' htmlColor='#cc9955' />
-              R&D Farm: Gangwon-do Hoengseong-gun Gonggeunmyeon Osanri 262-13
-            </p>
-            <p>
-              <PhoneIcon fontSize='small' htmlColor='#cc9955' />
-              (Tel) 010-2088-9798
-            </p>
-            <p>
-              <EmailIcon fontSize='small' htmlColor='#cc9955' />
-              trackfarm@naver.com
-            </p>
+          <div className="footer-description">
+            <div className="description-item">
+              <LocationIcon fontSize="inherit" htmlColor="#7FFC3B" />
+              <p className="font-text-sm">
+                HeadOffice: Chungcheongnam-do Asan-si Baebangeup Heemangro
+                46beongil 45-11, 502
+              </p>
+            </div>
+            <div className="description-item">
+              <LocationIcon fontSize="inherit" htmlColor="#7FFC3B" />
+              <p className="font-text-sm">
+                Laboratory: Gyeonggi-do Seongnam-si Sujeonggu Daewangpangyoro
+                815, Pangyo Startup-zone, 832
+              </p>
+            </div>
+            <div className="description-item">
+              <LocationIcon fontSize="inherit" htmlColor="#7FFC3B" />
+              <p className="font-text-sm">
+                R&D Farm: Gangwon-do Hoengseong-gun Gonggeunmyeon Osanri 262-13
+              </p>
+            </div>
+            <div className="description-item">
+              <PhoneIcon fontSize="inherit" htmlColor="#7FFC3B" />
+              <p className="font-text-sm">(Tel) 010-2088-9798</p>
+            </div>
+            <div className="description-item">
+              <EmailIcon fontSize="inherit" htmlColor="#7FFC3B" />
+              <p className="font-text-sm">trackfarm@naver.com</p>
+            </div>
           </div>
         </div>
-        <div>
-          <div className='footer-title'>
-            LINKS LIST
-          </div>
-          <div className='footer-description'>
-            <p>
-              Help Center
-            </p>
-            <p>
-              Community
-            </p>
-            <p>
-              Forums
-            </p>
-            <p>
-              Blog
-            </p>
-            <p>
-              Meetups
-            </p>
+        <div className="footer-description-container">
+          <div className="footer-description-title">LINKS LIST</div>
+          <div className="footer-description">
+            <div className="description-item">
+              <p className="font-text-sm">Help Center</p>
+            </div>
+            <div className="description-item">
+              <p className="font-text-sm">Community</p>
+            </div>
+            <div className="description-item">
+              <p className="font-text-sm">Forums</p>
+            </div>
+            <div className="description-item">
+              <p className="font-text-sm">Blog</p>
+            </div>
+            <div className="description-item">
+              <p className="font-text-sm">Meetups</p>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="footer-copyright-container">
+        <p>
+          COPYRIGHTⓒ2020
+          <br />
+          BY TRACKFARM CO, LTD. ALL RIGHTS RESERVED
+        </p>
       </div>
     </div>
   );
