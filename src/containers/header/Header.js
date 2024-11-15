e4dbefb02f5913dd9a2496e477e64500a15703fb
inf0craw1 <inf0craw1@naver.com>
2024-11-15 20:40:45 +0900

    refactor: Edit page name

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -31,8 +31,8 @@ const Header = ({ isMobile, isOpen, onClickMenu }) => {
               <>
                 <Link to="/">HOME</Link>
                 <Link to="/company">COMPANY</Link>
-                <Link to="/project">PROJECT</Link>
-                <Link to="/notice">NOTICE</Link>
+                <Link to="/project">PRODUCTS</Link>
+                <Link to="/notice">NEWS</Link>
               </>
             )}
           </div>
